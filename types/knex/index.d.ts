42dae44984d0edffb0c3ce6be55d36bf10d82e15
Jasmeet Oberai <jasoberai@gmail.com>
2018-12-22 13:01:51 -0500

    add sql native type for custom driver execution

--- types/knex/index.d.ts
+++ types/knex/index.d.ts
@@ -385,6 +385,12 @@ declare namespace Knex {
         options: any;
         bindings: Value[];
         sql: string;
+        toNative(): SqlNative;
+    }
+
+    interface SqlNative {
+        bindings: Value[];
+        sql: string;
     }
 
     //
